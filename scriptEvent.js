b0bce2a8a0f729e329dd7b16011ebf253b8aed71
comeChev <107626959+comeChev@users.noreply.github.com>
2022-06-28 17:17:57 +0200

    sorting dataEvent on PastEvent & PresentEvent

--- scriptEvent.js
+++ scriptEvent.js
@@ -1,55 +1,98 @@
+async function populateData(){
+  await fetch("./json/dataEvent.json")
+  .then((response)=>{
+    return response.json()
+  })
+  .then((dataJson)=>{
+    myDataEvent = dataJson
+    myDataEvent.forEach(element => {
+      let dateElement = new Date(element.dateEvent)
+      if (dateElement < dateNow){
+        myDataEventPast.push(element)
+      } else{
+        myDataEventNow.push(element)
+      }
+    });
+  })
+  .then(()=>{
+    myDataEventNow.sort((a,b)=>{
+      return new Date(a.dateEvent) - new Date(b.dateEvent)
+    })
+    myDataEventPast.sort((a,b)=>{
+      return new Date(a.dateEvent) - new Date(b.dateEvent)
+    })
+  })
+}
 
 let myModal = new bootstrap.Modal(document.querySelector("#modalContact"), {});
 let buttonModal = document.querySelector("#btnContact")
 let mdMediaQuery = window.matchMedia("(min-width: 768px)")
-let eventTarget = document.querySelector("#eventTarget")
+let eventTargetNow = document.querySelector("#eventTargetNow")
+let eventTargetPast = document.querySelector("#eventTargetPast")
 let myDataEvent=[];
+let myDataEventNow=[];
+let myDataEventPast=[];
+let dateNow= new Date()
 
-function populateMobile(){
-  fetch('./json/dataEvent.json')
-  .then((response)=>{
-    return response.json()
-  })
-  .then((data)=>{
-    myDataEvent=data
-  })
-  .then(()=>{
-    eventTarget.innerHTML = ""
-    myDataEvent.forEach(event => {
-    eventTarget.innerHTML += `<div class="card shadow-card col-12 mb-4">
-    <div class=" card-body ps-2 pe-2 col-12 bg-white text-dark rounded">
-    <h3 class="card-title mainFontFam">${event.nameEvent}</h3>
-    <h6 class="card-subtitle secondaryFontFam">${event.dateEvent}</h6>
-    <p class="card-text thirdFontFam">${event.descEvent}</p>
-    <button class="btn btn-orange float-end mainFontFam">S'incrire</button>
-    </div>
-    </div>`
+
+
+async function populateMobile(){
+    eventTargetNow.innerHTML = ""
+    myDataEventNow.forEach(event => {
+      let dateEvent = new Date(event.dateEvent)
+      eventTargetNow.innerHTML += `<div class="card shadow-card col-12 mb-4">
+      <div class=" card-body ps-2 pe-2 col-12 bg-white text-dark rounded">
+      <h3 class="card-title mainFontFam">${event.nameEvent}</h3>
+      <h6 class="card-subtitle secondaryFontFam">${dateEvent}</h6>
+      <p class="card-text thirdFontFam">${event.descEvent}</p>
+      <button class="btn btn-orange float-end mainFontFam">S'incrire</button>
+      </div>
+      </div>`
     });
-  })
-}
-function populate(){
-  fetch('./json/dataEvent.json')
-  .then((response)=>{
-    return response.json()
-  })
-  .then((data)=>{
-    myDataEvent = data
-  })
-  .then(()=>{
-    eventTarget.innerHTML = ""
-    myDataEvent.forEach(event => {
-    eventTarget.innerHTML += `<div class="col-md-4 col-lg-3 pb-2 pt-2">
+    eventTargetPast.innerHTML=""
+    myDataEventPast.forEach(event => {
+      let dateEvent = new Date(event.dateEvent)
+      eventTargetPast.innerHTML += `<div class="card shadow-card col-12 mb-4">
+      <div class=" card-body ps-2 pe-2 col-12 bg-white text-dark rounded">
+      <h3 class="card-title mainFontFam">${event.nameEvent}</h3>
+      <h6 class="card-subtitle secondaryFontFam">${dateEvent}</h6>
+      <p class="card-text thirdFontFam">${event.descEvent}</p>
+      <button class="btn btn-orange float-end mainFontFam">S'incrire</button>
+      </div>
+      </div>`
+    });
+
+  }
+async function populate(){
+  eventTargetNow.innerHTML = ""
+  myDataEventNow.forEach(event => {
+    let dateEvent = new Date(event.dateEvent)
+    eventTargetNow.innerHTML += `<div class="col-md-4 col-lg-3 pb-2 pt-2">
     <div class="card animateCard text-dark bg-light card-size">
     <img class="card-img-top" src= ${event.imgEvent}>
     <div class="card-body position-relative">
     <h3 class="card-title mainFontFam">${event.nameEvent}</h3>
-    <h6 class="card-subtitle secondaryFontFam">${event.dateEvent}</h6>
+    <h6 class="card-subtitle secondaryFontFam">${dateEvent}</h6>
     <p class="card-text thirdFontFam pb-4">${event.descEvent}</p>
     <button class="btn btn-orange btn-bot mainFontFam">S'incrire</button>
     </div>
     </div>
     </div>`
-    })
+  })
+  eventTargetPast.innerHTML = ""
+  myDataEventPast.forEach(event => {
+    let dateEvent = new Date(event.dateEvent)
+    eventTargetPast.innerHTML += `<div class="col-md-4 col-lg-3 pb-2 pt-2">
+    <div class="card animateCard text-dark bg-light card-size">
+    <img class="card-img-top" src= ${event.imgEvent}>
+    <div class="card-body position-relative">
+    <h3 class="card-title mainFontFam">${event.nameEvent}</h3>
+    <h6 class="card-subtitle secondaryFontFam">${dateEvent}</h6>
+    <p class="card-text thirdFontFam pb-4">${event.descEvent}</p>
+    <button class="btn btn-orange btn-bot mainFontFam">S'incrire</button>
+    </div>
+    </div>
+    </div>`
   })
 }
 function showModal(){
@@ -57,9 +100,12 @@ function showModal(){
 }
 
 
+
 if(mdMediaQuery.matches){
+  populateData()
   populate()
 } else{
+  populateData()
   populateMobile()
 }
 buttonModal.addEventListener("click",showModal);
@@ -70,6 +116,7 @@ mdMediaQuery.addEventListener("change",()=>{
     populateMobile()
   }
 });
+
 
 function underConstruction(){
   alert(`Cette page est en cours de construction. Veuillez nous excuser pour la gêne occasionnée.`)
