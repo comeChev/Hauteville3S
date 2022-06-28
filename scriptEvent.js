e8f7137bc09c63beb9730b04e1f82618e59d1543
comeChev <107626959+comeChev@users.noreply.github.com>
2022-06-28 12:33:52 +0200

    fixed problem loading cards event

--- scriptEvent.js
+++ scriptEvent.js
@@ -5,35 +5,37 @@ let mdMediaQuery = window.matchMedia("(min-width: 768px)")
 let eventTarget = document.querySelector("#eventTarget")
 let myDataEvent=[];
 
-// on récupére les donnnées du fichier JSON
-fetch("./json/dataEvent.json")
-.then(function(response){
-  // on récupère la réponse et on la rend lisible en JS
-  return response.json();
-})
-.then(function(response2){
-  // on récupère les données lisibles et on les affiche dans la console
-  myDataEvent = response2
-  //console.log(myDataEvent)
-})
-
-// fonction pour créer des objets HTML en fonction des datas
 function populateMobile(){
-  eventTarget.innerHTML = ""
-  myDataEvent.forEach(event => {
-  eventTarget.innerHTML += `<div class="card col-12 m-2">
-  <div class=" card-body ps-2 pe-2 col-12 bg-white text-dark rounded">
-  <h3 class="card-title mainFontFam">${event.nameEvent}</h3>
-  <h6 class="card-subtitle secondaryFontFam">${event.dateEvent}</h6>
-  <p class="card-text thirdFontFam">${event.descEvent}</p>
-  <button class="btn btn-orange float-end mainFontFam">S'incrire</button>
-  </div>
-  </div>`
- });
+  fetch('./json/dataEvent.json')
+  .then((response)=>{
+    return response.json()
+  })
+  .then((data)=>{
+    myDataEvent=data
+  })
+  .then(()=>{
+    eventTarget.innerHTML = ""
+    myDataEvent.forEach(event => {
+    eventTarget.innerHTML += `<div class="card col-12 m-2">
+    <div class=" card-body ps-2 pe-2 col-12 bg-white text-dark rounded">
+    <h3 class="card-title mainFontFam">${event.nameEvent}</h3>
+    <h6 class="card-subtitle secondaryFontFam">${event.dateEvent}</h6>
+    <p class="card-text thirdFontFam">${event.descEvent}</p>
+    <button class="btn btn-orange float-end mainFontFam">S'incrire</button>
+    </div>
+    </div>`
+    });
+  })
 }
 function populate(){
-    if (!myDataEvent=="" |!myDataEvent==null){
-    console.log(`ca va charger`)
+  fetch('./json/dataEvent.json')
+  .then((response)=>{
+    return response.json()
+  })
+  .then((data)=>{
+    myDataEvent = data
+  })
+  .then(()=>{
     eventTarget.innerHTML = ""
     myDataEvent.forEach(event => {
     eventTarget.innerHTML += `<div class="col-md-4 col-lg-3 pb-2 pt-2">
@@ -47,14 +49,18 @@ function populate(){
     </div>
     </div>
     </div>`
-    })}
-    else{console.log(`données vides`)}
-    }
+    })
+  })
+}
 function showModal(){
   myModal.show();
 }
 
-populate()
+if(mdMediaQuery.matches){
+  populate()
+} else{
+  populateMobile()
+}
 buttonModal.addEventListener("click",showModal);
 mdMediaQuery.addEventListener("change",()=>{
   if(mdMediaQuery.matches){
